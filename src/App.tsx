a05f27e333b6ce138985fadf4aef4707c5890b1b
Ma6upa <vaud55@gmail.com>
2023-05-25 00:34:52 +0400

    add RegistrationPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
       </Routes>
     </>
   )
