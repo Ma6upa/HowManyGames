af498b3d9984a6ac7df7ff6848d392df33c2f5da
Ma6upa <vaud55@gmail.com>
2023-05-31 20:25:07 +0400

    add please log in modal

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { gamesAPI } from "../store/api/gamesApi";
 import {
@@ -6,16 +6,22 @@ import {
   Button,
   Container,
   CssBaseline,
-  TextField,
   ThemeProvider,
   Typography,
   createTheme,
+  Modal,
+  Divider
 } from "@mui/material"
 
 const GamePage = () => {
   const theme = createTheme();
   const { id } = useParams();
   const { data: game, isLoading } = gamesAPI.useGetGameQuery(Number(id))
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleClose = () => {
+    setOpenModal(false)
+  }
 
   useEffect(() => {
     console.log(game)
@@ -51,9 +57,78 @@ const GamePage = () => {
             <Typography component="h1" variant="h4">
               {game?.name}
             </Typography>
+            <Box sx={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column'
+            }}>
+              <Box sx={{
+                width: '20%',
+                left: 0,
+                display: 'flex',
+                flexDirection: 'column'
+              }}>
+                <img src={import.meta.env.VITE_API + `/${game?.picturePath}`} style={{
+                  width: '100%',
+                }} alt="No picture" />
+              </Box>
+              <Box sx={{
+                width: '70%',
+                right: 0
+              }}>
+                <Button
+                  onClick={() => setOpenModal(true)}
+                >
+                  Add to list +
+                </Button>
+              </Box>
+            </Box>
           </Box>
         )}
       </Container>
+      <Modal
+        open={openModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Container component="main" maxWidth="sm">
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            marginTop: '10%',
+            borderRadius: 5,
+            height: 300
+          }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography variant="h6">
+                Please log in
+              </Typography>
+              <Divider />
+              <Box
+                sx={{
+                  width: '90%',
+                  alignItems: 'center',
+                  textAlign: 'center',
+                  marginTop: 10
+                }}
+              >
+                <Typography variant="h4">
+                  You have to be logged in to use this option
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Container>
+      </Modal>
     </ThemeProvider>
   )
 }
