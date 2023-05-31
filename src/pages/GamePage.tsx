69828fc4dabfb3c47b91ff0995a49c628db1c5c2
Ma6upa <vaud55@gmail.com>
2023-05-31 19:27:21 +0400

    add is loading to game page

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,9 +1,60 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { gamesAPI } from "../store/api/gamesApi";
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material"
 
 const GamePage = () => {
+  const theme = createTheme();
   const { id } = useParams();
-  return(
-    <div>Game page {id}</div>
+  const { data: game, isLoading } = gamesAPI.useGetGameQuery(Number(id))
+
+  useEffect(() => {
+    console.log(game)
+  }, [game])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="lg">
+        <CssBaseline />
+        {isLoading && (
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Typography component="h1" variant="h4">
+              Идет загрузка...
+            </Typography>
+          </Box>
+        )}
+        {!isLoading && (
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Typography component="h1" variant="h4">
+              {game?.name}
+            </Typography>
+          </Box>
+        )}
+      </Container>
+    </ThemeProvider>
   )
 }
 
